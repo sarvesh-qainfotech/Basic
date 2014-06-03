f67dd57d095a3bfba8e807a0c505062f17a6f1a1
Sarvesh Shrivastava <SarveshShrivastava@qainfotech.net>
2014-06-03 08:44:01 +0530

    Create a new class BaseFixture

--- src/com/qainfotech/runtest/AutomatedTatocTestngRun.java
+++ src/com/qainfotech/runtest/AutomatedTatocTestngRun.java
@@ -1,42 +1,34 @@
 package com.qainfotech.runtest;
+
 import org.testng.annotations.AfterClass;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import com.qainfotech.testobjects.AutomatesFifthLevel;
-import com.qainfotech.testobjects.AutomatesFirstLevel;
-import com.qainfotech.testobjects.AutomatesFourthLevel;
-import com.qainfotech.testobjects.AutomatesSecondLevel;
-import com.qainfotech.testobjects.AutomatesThirdLevel;
-import com.qainfotech.testobjects.Browser;
 
-public class AutomatedTatocTestngRun {
 
-	Browser browser = new Browser();
-	AutomatesFirstLevel level1 = new AutomatesFirstLevel();
-	AutomatesSecondLevel level2 = new AutomatesSecondLevel();
-	AutomatesThirdLevel level3 = new AutomatesThirdLevel();
-	AutomatesFourthLevel level4 = new AutomatesFourthLevel();
-	AutomatesFifthLevel level5 = new AutomatesFifthLevel();
+
+public class AutomatedTatocTestngRun  {
+
+	BaseFixture basefixture = new BaseFixture();
 
 	@BeforeClass
 	public void beforeClass() {
-		browser.openBrowser();
-		browser.setUrl("http://172.16.1.17/tatoc");
 
+		basefixture.initializer();
 	}
 
 	@Test
 	public void tetscase1() {
-		level1.automatingFirstLevel(browser.driver);
-		level2.automatingSecondLevel(browser.driver);
-		level3.automatingThirdLevel(browser.driver);
-		level4.automatingFourthLevel(browser.driver);
-		level5.automatingFifthLevel(browser.driver);
+		
+		basefixture.runTestCases();
+
 	}
 
 	@AfterClass
 	public void afterClass() {
-		browser.closeBrowser();
+		
+		basefixture.closer();
+
 	}
 
 }
