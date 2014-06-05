580cae864b6e3d55d4d804b0a268f92a7c8d5230
Sarvesh Shrivastava <SarveshShrivastava@qainfotech.net>
2014-06-05 09:31:14 +0530

    Remove unnecessary comments

--- src/com/qainfotech/runtest/AutomatedTatocTestngRun.java
+++ src/com/qainfotech/runtest/AutomatedTatocTestngRun.java
@@ -11,14 +11,14 @@ public class AutomatedTatocTestngRun  {
 
 	BaseFixture basefixture = new BaseFixture();
 
-	// it wll run before the class
+	
 	@BeforeClass
 	public void beforeClass() {
 
 		basefixture.initializer();
 	}
 
-	// test cases
+	
 	@Test
 	public void tetscase1() {
 		
@@ -26,7 +26,7 @@ public class AutomatedTatocTestngRun  {
 
 	}
 
-	// it will run after the class
+	
 	@AfterClass
 	public void afterClass() {
 		
